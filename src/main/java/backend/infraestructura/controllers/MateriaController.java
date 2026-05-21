513c66cc2a9fee0655464cb731640c0fbddef2f6
Lucas Lopez <lflopez@gylgroup.com>
2026-05-21 15:00:47 -0300

    fix/2105/Se agrego anotacion @CrossOrigin a MateriaController

--- src/main/java/backend/infraestructura/controllers/MateriaController.java
+++ src/main/java/backend/infraestructura/controllers/MateriaController.java
@@ -15,6 +15,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/materias")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class MateriaController {
 
     private final MateriaService materiaService;
