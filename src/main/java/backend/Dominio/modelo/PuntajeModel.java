f082db70f7bb65ce5b771d150e5b8e2ecf3aa6cf
Jorge <jigaitan@gylgroup.com>
2026-05-21 14:27:03 -0300

    feat: Se agregaron los setters y getters de MateriaModel y PuntajeModel, tambien se les borro las notaciones de Lombok

--- src/main/java/backend/Dominio/modelo/PuntajeModel.java
+++ src/main/java/backend/Dominio/modelo/PuntajeModel.java
@@ -1,16 +1,13 @@
 package backend.Dominio.modelo;
 
-
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
 public class PuntajeModel {
     private Long id;
     private double valor;
     private EstudianteModel estudiante;
     private MateriaModel materia;
+
+    public PuntajeModel() {
+    }
 
     public PuntajeModel(Integer valor, EstudianteModel estudiante, MateriaModel materia) {
         this.valor = valor;
@@ -18,12 +15,36 @@ public class PuntajeModel {
         this.materia = materia;
     }
 
-
-    public void validar() {
-        if (valor < 0 || valor > 10) {
-            throw new IllegalArgumentException("El puntaje debe estar entre 0 y 10");
-        }
+    public Long getId() {
+        return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public EstudianteModel getEstudiante() {
+        return estudiante;
+    }
+
+    public void setEstudiante(EstudianteModel estudiante) {
+        this.estudiante = estudiante;
+    }
+
+    public MateriaModel getMateria() {
+        return materia;
+    }
+
+    public void setMateria(MateriaModel materia) {
+        this.materia = materia;
+    }
 
 }
