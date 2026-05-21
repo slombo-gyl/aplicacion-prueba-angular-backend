8a27f1c647377ddcff763c1910f7af6c957ae414
Leonardo L. Scuccimarra <dllopez@gylgroup.com>
2026-05-21 11:10:34 -0300

    HOTFIX / 21 05 Se corrige error de repositorio Puntaje

--- src/main/java/backend/infraestructura/repositorios/JpaPuntajeRepository.java
+++ src/main/java/backend/infraestructura/repositorios/JpaPuntajeRepository.java
@@ -6,11 +6,7 @@ import backend.infraestructura.entidades.MateriaEntity;
 import backend.infraestructura.entidades.PuntajeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface JpaPuntajeRepository extends JpaRepository<PuntajeEntity,Long> {
     boolean existsByEstudianteAndMateria(EstudianteEntity estudiante, MateriaEntity materia);
     PuntajeModel findByEstudianteAndMateria(EstudianteEntity estudiante, MateriaEntity materia);
-    List<PuntajeEntity> findByFechaBajaIsNull();
-    List<PuntajeEntity> findByFechaBajaIsNotNull();
 }
