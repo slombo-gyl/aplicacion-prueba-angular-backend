ed492889d7b4bb7b66fafc7304f255d756ddfb56
Josue Trujillo <jtrujillo@gylgroup.com>
2026-05-26 13:25:45 -0300

    fix: correcion del chart

--- src/main/java/backend/Aplicacion/usecase/puntaje/ObtenerPuntaje.java
+++ src/main/java/backend/Aplicacion/usecase/puntaje/ObtenerPuntaje.java
@@ -1,19 +1,23 @@
 package backend.Aplicacion.usecase.puntaje;
 
 import backend.Aplicacion.dto.puntaje.ObtenerPuntajesDTOResponse;
+import backend.Dominio.modelo.PuntajeModel;
+import backend.Dominio.puertos.out.puntaje.PuntajeRepositoryPort;
 import backend.Infraestructura.output.persistencia.repository.puntaje.PuntajeJpaRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@AllArgsConstructor
 public class ObtenerPuntaje {
 
-    PuntajeJpaRepository puntajeJpaRepository;
+    private final PuntajeRepositoryPort puntajeRepositoryPort;
 
     public ObtenerPuntajesDTOResponse ejecutar (){
-        var puntajes = puntajeJpaRepository.findAll();
+        List<PuntajeModel> puntajes = puntajeRepositoryPort.listarTodos();
 
         List<String> labels = puntajes.stream()
                 .map(p -> p.getMateria().getNombre())
