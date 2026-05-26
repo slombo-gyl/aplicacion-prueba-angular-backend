2f7ed86f4f12be3ffc3490f860c8df793bccea6c
Jorge <jigaitan@gylgroup.com>
2026-05-26 11:39:53 -0300

    feat: Se reimplemento los casos de uso del chart y su endpoint para el consumo del front

--- src/main/java/backend/Aplicacion/usecase/puntaje/ListarPuntajeUseCase.java
+++ src/main/java/backend/Aplicacion/usecase/puntaje/ListarPuntajeUseCase.java
@@ -1,15 +1,12 @@
 package backend.Aplicacion.usecase.puntaje;
 
-import backend.Aplicacion.dto.puntaje.PuntajesDTOResponse;
 import backend.Dominio.modelo.PuntajeModel;
 import backend.Dominio.puertos.in.puntaje.ListarPuntaje;
 import backend.Dominio.puertos.out.puntaje.PuntajeRepositoryPort;
-import backend.Infraestructura.output.persistencia.repository.puntaje.PuntajeJpaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
---
+++ src/main/java/backend/Aplicacion/usecase/puntaje/ObtenerPuntaje.java
@@ -0,0 +1,26 @@
+package backend.Aplicacion.usecase.puntaje;
+
+import backend.Aplicacion.dto.puntaje.ObtenerPuntajesDTOResponse;
+import backend.Infraestructura.output.persistencia.repository.puntaje.PuntajeJpaRepository;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+public class ObtenerPuntaje {
+
+    PuntajeJpaRepository puntajeJpaRepository;
+
+    public ObtenerPuntajesDTOResponse ejecutar (){
+        var puntajes = puntajeJpaRepository.findAll();
+
+        List<String> labels = puntajes.stream()
+                .map(p -> p.getMateria().getNombre())
+                .collect(Collectors.toList());
+
+        List<Integer> data = puntajes.stream()
+                .map(p -> (int) p.getValor())
+                .collect(Collectors.toList());
+
+        return new ObtenerPuntajesDTOResponse(labels,data);
+    }
+}
